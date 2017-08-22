18d28cb5de1c1daa96fd2eee5de117c0536e0aae
Laura Hughes <flaneuse@users.noreply.github.com>
2017-08-22 14:37:04 -0400

    allow inputs of strings

--- R/loadDHS.R
+++ R/loadDHS.R
@@ -8,24 +8,38 @@
 #' Country codes: \link{http://dhsprogram.com/data/File-Types-and-Names.cfm#CP_JUMP_10136}
 #' Indicator codes: \link{http://api.dhsprogram.com/rest/dhs/indicators?returnFields=IndicatorId,Label,Definition&f=html}
 #' @param breakdown one of 'national', 'subnational', 'background', or 'all'
-#' @param indicators: list of DHS indicator codes as a string separated by commas *NO SPACES between*, e.g. 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
+#' @param indicators: either a list of indicators to search for, or a string of DHS indicator codes separated by commas *NO SPACES between*, e.g. 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
 #' @param countries: list of DHS country names as a string separated by commas with *NO SPACES between*, e.g. 'SN,SL,TG'
-#' @param years: list of survey years to include as a string separated by commas, e.g. '2010,2012,2014'
-#' @param apiKey: API Key to include, to be able to obtain 5000 records instead of 1000.
+#' @param years: list of survey years to include as a string separated by commas, e.g. '2010,2012,2014'. By default, will return all available years.
+#' @param apiKey: API Key to include, to be able to obtain 5000 records instead of 1000 at a time.
 #' @param numResults: number of records to include
 #'
-#' @import dplyr RJSONIO
+#' @author Laura Hughes
+#'
+#' @import dplyr RJSONIO stringr
+#' @export
 #'
 #' @examples
-#' rw_stunting = loadDHS(breakdown = 'national', indicators = 'CN_NUTS_C_HA2', countries = 'RW')
-#' @author Laura Hughes
+#' rw_stunting = loadDHS(breakdown = 'all', indicators = 'CN_NUTS_C_HA2,CN_NUTS_C_HA3', countries = 'RW', years = '2010,2015')
+#' rw_stunting = loadDHS(breakdown = 'subnational', indicators = 'stunted', countries = 'Rwanda')
+#' malnutrition = loadDHS(breakdown = 'national', indicators = c('stunted', 'wasted', 'underweight'), countries = c('Rwanda', 'Burundi', 'Niger'))
+
 
 loadDHS = function(breakdown = "national",
                    indicators,
                    countries,
                    years = paste0(seq(1984,
                                       as.numeric(format(Sys.Date(), "%Y"))), collapse = ","), apiKey = NA, numResults = 1000) {
+  if(length(countries) > 1) {
+    countries = getDHScountry(countries)
+  }
 
+
+  if(length(indicators) > 1) {
+    indicators = getDHSindicator(indicators)
+  }
+
+  # define the query
   query = paste0("http://api.dhsprogram.com/rest/dhs/data?breakdown=",
                  breakdown, "&indicatorIds=", paste(indicators, collapse = ','), "&countryIds=", paste(countries, collapse = ','), "&SurveyYear=",
                  years, "&apiKey=", apiKey, "&perpage=", numResults)
