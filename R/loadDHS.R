d248e3e4688658ee20606d3eb6d0b3654c30d5ac
Laura Hughes <flaneuse@users.noreply.github.com>
2017-08-24 14:40:46 -0400

    start/end year; input string or list of countries/indicators

--- R/loadDHS.R
+++ R/loadDHS.R
@@ -8,8 +8,8 @@
 #' Country codes: \link{http://dhsprogram.com/data/File-Types-and-Names.cfm#CP_JUMP_10136}
 #' Indicator codes: \link{http://api.dhsprogram.com/rest/dhs/indicators?returnFields=IndicatorId,Label,Definition&f=html}
 #' @param breakdown one of 'national', 'subnational', 'background', or 'all'
-#' @param indicators: either a list of indicators to search for, or a string of DHS indicator codes separated by commas *NO SPACES between*, e.g. 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
-#' @param countries: list of DHS country names as a string separated by commas with *NO SPACES between*, e.g. 'SN,SL,TG'
+#' @param indicators: either a list of indicator names/codes to search for, or a string of DHS indicator names/codes separated by commas, e.g. 'CN_NUTS_C_WH2,CN_NUTS_C_HA2,ED_EDUC_W_SEP'
+#' @param countries: either a list of DHS country names or codes, or a string of country names/codes separated by commas, e.g. 'SN,SL,TG'
 #' @param years: list of survey years to include as a string separated by commas, e.g. '2010,2012,2014'. By default, will return all available years.
 #' @param apiKey: API Key to include, to be able to obtain 5000 records instead of 1000 at a time.
 #' @param numResults: number of records to include
@@ -21,7 +21,7 @@
 #'
 #' @examples
 #' rw_stunting = loadDHS(breakdown = 'all', indicators = 'CN_NUTS_C_HA2,CN_NUTS_C_HA3', countries = 'RW', years = '2010,2015')
-#' rw_stunting = loadDHS(breakdown = 'subnational', indicators = 'stunted', countries = 'Rwanda')
+#' rw_stunting = loadDHS(breakdown = 'subnational', indicators = 'stunted', countries = 'Rwanda', start_year = 2005, end_year = 2015)
 #' malnutrition = loadDHS(breakdown = 'national', indicators = c('stunted', 'wasted', 'underweight'), countries = c('Rwanda', 'Burundi', 'Niger'))
 
 
@@ -33,50 +33,96 @@ loadDHS = function(breakdown = "national",
                    years = NA,
                    apiKey = NA, numResults = 1000,
                    return_params = FALSE) {
-  if(length(countries) > 1) {
-    countries = getDHScountry(countries)
+
+
+  # helper function to define countries and indicators
+  define_codes = function(input, lookup_table, code_var, input_type = c('countries', 'indicators')) {
+
+    input_string = str_replace_all(paste(input, collapse = ','), ", ", ",")
+    input_list = lapply(str_split(input_string, pattern = ','), str_trim)
+
+    # check if codes match
+    code_match = input_list[[1]] %in% lookup_table[[code_var]]
+
+    if(any(code_match)) {
+      # codes inputted
+      output = input_string
+
+      # check if all codes match
+      if(!all(code_match)) {
+        warning(paste(input_type, input_list[[1]][!code_match],
+                      "were not found in DHS. Check the codes you inputted, or try matching by name."))
+      }
+    } else{
+      if(input_type == 'countries'){
+        output = getDHScountry(input_list[[1]])
+      } else {
+        output = getDHSindicator(input_list[[1]])
+      }
+    }
+
+    return(output)
   }
 
+  # -- define countries --
+  countries = define_codes(countries, DHScountries, 'code', 'countries')
 
-  if(length(indicators) > 1) {
-    indicators = getDHSindicator(indicators)
+
+  if(is.null(countries)) {
+    warning("Exiting; no countries found")
+    return(NULL)
   }
 
-  # define the query
-  query = paste0("http://api.dhsprogram.com/rest/dhs/data?breakdown=",
-                 breakdown, "&indicatorIds=", paste(indicators, collapse = ','), "&countryIds=", paste(countries, collapse = ','), "&SurveyYear=",
-                 years, "&apiKey=", apiKey, "&perpage=", numResults)
+  # -- define indicators --
+  indicators = define_codes(indicators, DHSindic, 'IndicatorId', 'indicators')
 
-  json_file = fromJSON(query)
-
-  # Unlist the JSON file entries
-  json_data = lapply(json_file$Data, function(x) {
-    unlist(x)
-  })
-
-  # Convert JSON input to a data frame
-  df = as.data.frame(do.call("rbind", json_data), stringsAsFactors = FALSE)
-
-
-  # Throw a warning if number of returned results > numResults
-  if (json_file$RecordCount > numResults) {
-    warning(paste0("query results in ", json_file$RecordCount, " hits; first ", numResults,
-                   " returned"))
+  if(is.null(indicators)) {
+    warning("Exiting; no indicators found")
+    return(NULL)
   }
 
-  # Check that everything are numbers.  grepl('^[[:digit:]]',y$Indicator)
-
-  # Check that it returns a value.
-  if (length(df) > 0) {
-    # Convert values to numbers.
-    df = df %>% mutate_at(.funs = as.numeric, .vars = vars(Value, Precision, SurveyYear, IsTotal,
-                                                           CILow, CIHigh, DenominatorUnweighted, DenominatorWeighted))
-  }
-
-  if(return_params == TRUE) {
-    return(list(data = df, indicators = indicators, countries = countries, breakdown = breakdown))
-
+  # -- define years --
+  if(is.na(years)) {
+    years = paste(seq(start_year, end_year), collapse = ",")
   } else {
-    return(df)
+    years = str_replace_all(years, ", ", ",")
   }
+
+# define the query
+query = paste0("http://api.dhsprogram.com/rest/dhs/data?breakdown=",
+               breakdown, "&indicatorIds=", indicators, "&countryIds=", countries, "&SurveyYear=",
+               years, "&apiKey=", apiKey, "&perpage=", numResults)
+
+json_file = fromJSON(query)
+
+# Unlist the JSON file entries
+json_data = lapply(json_file$Data, function(x) {
+  unlist(x)
+})
+
+# Convert JSON input to a data frame
+df = as.data.frame(do.call("rbind", json_data), stringsAsFactors = FALSE)
+
+
+# Throw a warning if number of returned results > numResults
+if (json_file$RecordCount > numResults) {
+  warning(paste0("query results in ", json_file$RecordCount, " hits; first ", numResults,
+                 " returned"))
+}
+
+# Check that everything are numbers.  grepl('^[[:digit:]]',y$Indicator)
+
+# Check that it returns a value.
+if (length(df) > 0) {
+  # Convert values to numbers.
+  df = df %>% mutate_at(.funs = as.numeric, .vars = vars(Value, Precision, SurveyYear, IsTotal,
+                                                         CILow, CIHigh, DenominatorUnweighted, DenominatorWeighted))
+}
+
+if(return_params == TRUE) {
+  return(list(data = df, indicators = indicators, countries = countries, breakdown = breakdown))
+
+} else {
+  return(df)
+}
 }
