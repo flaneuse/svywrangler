0bbdd1050e33d9c9a32fd98d929d92e83b375c80
Laura Hughes <flaneuse@users.noreply.github.com>
2017-09-21 11:30:24 -0400

    update to auto-pull dicts

--- R/factorize.R
+++ R/factorize.R
@@ -1,11 +1,11 @@
 #' Create factor levels based on labels from a dataset
-#' 
+#'
 #' Reads labels from a reference dataset (usually imported from Stata, SPSS, or SAS)
 #' and applies them to the same variable in a label-free dataset to create a factor variable
 #' with levels and labels. By default, the levels will be sorted by their frequency in the dataset.
 
 #' @author Laura Hughes, laura.d.hughes@gmail.com
-#' 
+#'
 #' @param df main data frame containing the dataset to modify
 #' @param ref_df reference labelled data frame whose labels you want to use
 #' @param var string containing the variable name you want to change to a factor
@@ -13,36 +13,35 @@
 #'
 # @import dplyr
 
-factorize = function(df, ref_df, var, new_var) {
+factorize = function(df, var, new_var) {
+  codebk = pull_labels(df)
+
   # ref_df has labels associated with it.
   # Note: can pipe with magrittr pipe, a la: df %>% factorize(ref_df, var, new_var)
-  
+
   # -- check var is within both df and ref_df --
   if(!var %in% colnames(df)) {
     stop('variable is not in the current dataset (argument df)')
   }
-  
-  if(!var %in% colnames(ref_df)) {
+
+  if(!var %in% codebk$var_name) {
     stop('variable is not in the reference dataset (argument ref_df)')
   }
-  
+
   # -- pull out the label values --
-  codebk = data.frame(code = attr(ref_df[[var]], 'labels'))
-  
-  # -- pull out the label names --
-  codebk = codebk %>% mutate(names =  row.names(codebk))
-  
-  # -- create a factor with the labels from the original dataset -- 
+  codebk = codebk %>% filter(var_name == var)
+
+  # -- create a factor with the labels from the original dataset --
   # levels will be sorted by the frequency of occurance (high to low)
-  df = df %>% 
+  df = df %>%
     mutate_(.dots = setNames(
       list(paste0('forcats::fct_infreq(
                   factor(', var, ',',
-                  'levels = ', list(codebk$code), ',',
-                  'labels = ', list(codebk$names),'))'
-                  )), new_var 
+                  'levels = ', list(codebk$codes), ',',
+                  'labels = ', list(codebk$labels),'))'
+                  )), new_var
       ))
-  
+
   return(df)
 }
 
