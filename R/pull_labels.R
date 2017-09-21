b2c9253f9f0c391b106d3c2e7d61854d1dbfbc39
Laura Hughes <flaneuse@users.noreply.github.com>
2017-09-21 11:30:10 -0400

    updated col name to var_name

--- R/pull_labels.R
+++ R/pull_labels.R
@@ -1,19 +1,19 @@
 pull_labels = function(df) {
   cols = colnames(df)
-  
+
   get_labels = function(df, col) {
     label_name = attr(df[[col]], 'label')
-    
+
     labels = attr(df[[col]], 'labels')
-    
+
     if (is.null(labels)) {
       return(NA)
     } else{
       data.frame(descrip = label_name, codes = labels) %>% mutate(labels = row.names(.))
     }
   }
-  
-  lapply(cols, function(x) data.frame(var = x, get_labels(df, x)))  %>% bind_rows() %>% select(-`get_labels.df..x.`)
+
+  lapply(cols, function(x) data.frame(var_name = x, get_labels(df, x)))  %>% bind_rows() %>% select(-`get_labels.df..x.`)
 }
 
 library(stringr)
@@ -21,16 +21,16 @@ library(data.table)
 
 id_weirdos = function(df, lookfor = c('or more', 'unknown', "don't"), lookfor_codes = c(8, 9, 95:99, 996:999, 9996:9999)) {
   codebk = pull_labels(df)
-  
-  codebk %>% 
-    mutate(labels = str_to_lower(labels)) %>% 
+
+  codebk %>%
+    mutate(labels = str_to_lower(labels)) %>%
     # filter( )
     filter_(paste0(paste(paste0('labels %like% "', lookfor, '"'), collapse = '|'), '| codes %in% c(', paste(lookfor_codes, collapse = ','), ")"))
 }
 
 id_decimals = function(df) {
   codebk = pull_labels(df)
-  
-  codebk %>% 
+
+  codebk %>%
     filter(descrip %like% 'decimals')
 }
